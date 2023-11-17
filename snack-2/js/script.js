d89d3adc513055e53afb47627a7e323e1ed4a499
Donato-Carola <don94@live.it>
2023-11-17 12:03:12 +0100

    primo push stack-2

---
+++ snack-2/js/script.js
@@ -0,0 +1,9 @@
+//Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
+//e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+
+const name=["gigino","franco","pippo","topolino"];
+const lastname=["pomodoro","cornetto","nutella","crema"];
+
+// prendo un nome e un cognome dalle due liste create e creo la lista falsa finche non arriva alla fine delle due liste
+//cioè alla lunghezza totale della lista
