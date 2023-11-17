e6ef317d52b6b9f6e8c7d032c924b7abdbbd6bb1
Donato-Carola <don94@live.it>
2023-11-17 12:09:27 +0100

    macropensiero sull'esercizio

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -7,3 +7,14 @@ const lastname=["pomodoro","cornetto","nutella","crema"];
 
 // prendo un nome e un cognome dalle due liste create e creo la lista falsa finche non arriva alla fine delle due liste
 //cioè alla lunghezza totale della lista
+
+const listFalse=[];
+let i=0
+
+//finche i è minore della lunghezza di name && lastname 
+ //rimuovi dalla lista name e dalla lista lastname
+ //e fai push in listaFalse
+
+ i++
+
+ console.log(listFalse,i)
