8c6a83b9829b190d26facb8959ce67046863414d
Donato-Carola <don94@live.it>
2023-11-20 12:05:34 +0100

    prova finale

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -5,7 +5,7 @@ parole abbiano la stessa lunghezza.
 Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.*/
 
 
-const firstWord=prompt('inserisci parola').length;
+/*const firstWord=prompt('inserisci parola').length;
 const secondWord=prompt('inserisci seconda parola').length;
 
 function word (firstWord, secondWord){
@@ -13,10 +13,40 @@ function word (firstWord, secondWord){
         console.log(firstWord,secondWord)
        } else{
         return firstWord;
-        }
+       }
         
 
      
 }
 
-word(firstWord,secondWord);
+word(firstWord,secondWord);*/
+
+const firstWord=prompt('inserisci parola');
+const secondWord=prompt('inserisci seconda parola');
+
+
+if(word(firstWord,secondWord)){         //firstword e secondword sostituiscono wordOne e wordTwo nella funzione 
+                                        //in questo caso chiamiamo la funzione e gli diamo i due parametri che li sostituisce 
+    console.log(firstWord,secondWord)
+}else if(firstWord.length>secondWord.length){
+    console.log(firstWord);
+
+}else{
+    console.log(secondWord)
+}
+
+
+
+
+
+
+function word (wordOne,wordTwo){
+     if (wordOne.length === wordTwo.length){
+        return true;
+       } else{
+        return false;
+       }
+        
+
+     
+}
