360f64812054050919c00aaddad203b79ddc29ce
Donato-Carola <don94@live.it>
2023-11-21 12:14:01 +0100

    primo ragionamento

---
+++ snack-6/js/script.js
@@ -0,0 +1,16 @@
+
+//! Chiedo all'utente un numero e genero attraverso una funzione un numero di 
+//! quadrati equivalente al numero inserito dall'utente nel DOM, in qualsiasi forma. 
+
+
+const numberUser =parseInt(prompt('inserisci numero'));
+
+const box = document.querySelector('article')
+console.log(box)
+
+
+function number(){
+    for(let i=numberUser;i<=numberUser;i++){
+        
+    }
+}
