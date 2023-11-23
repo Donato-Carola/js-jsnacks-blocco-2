1c11505b5653788fd46a3e7b489646b54b9bb68d
Donato-Carola <don94@live.it>
2023-11-23 12:24:30 +0100

    struttura del codice prima di inserire le timing functions

---
+++ crono-snack/js/script.js
@@ -0,0 +1,11 @@
+
+
+//! Cliccando su un pulsante, viene avviato un cronometro. Per fermare il cronometro, bisogna cliccare su un secondo pulsante
+
+const button =document.getElementById('startButton')
+const buttonStop =document.getElementById('stopButton')
+const time=document.querySelector('h1')
+
+button.addEventListener('click',function(){
+        //inserisco setinterval in questo click
+})
