4c57bf12e25f8bbdbb09ec0f20bb9e4d9dc0ccc2
Donato-Carola <don94@live.it>
2023-11-23 12:47:37 +0100

    esercizio snack crono da concludere

--- crono-snack/js/script.js
+++ crono-snack/js/script.js
@@ -2,10 +2,21 @@
 
 //! Cliccando su un pulsante, viene avviato un cronometro. Per fermare il cronometro, bisogna cliccare su un secondo pulsante
 
-const button =document.getElementById('startButton')
+const buttonStart =document.getElementById('startButton')
 const buttonStop =document.getElementById('stopButton')
-const time=document.querySelector('h1')
+const timeSecond=document.getElementById('seconds')
 
-button.addEventListener('click',function(){
-        //inserisco setinterval in questo click
-})
+let clock;
+
+buttonStart.addEventListener('click',function(){
+           //inserisco setinterval in questo click per far partire il cronometro 
+         
+clock =setInterval(function(){
+    timeSecond.innerHTML=parseInt(timeSecond.innerText,10)+1;
+},1000);
+   });
+buttonStop.addEventListener('click',function(){
+    clearInterval(clock);
+});
+
+ 
