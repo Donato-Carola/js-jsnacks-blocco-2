6903241cf6f0cef7b352ebf0f8daca61866d8f28
Donato-Carola <don94@live.it>
2023-11-24 12:32:30 +0100

    push prima parte dell'esercizio - creazione 10 oggetti

---
+++ snack-7-zucchine/js/script.js
@@ -0,0 +1,63 @@
+
+
+//Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, 
+//peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+
+var zucchine = [
+    zucchineNere = {
+      varietà: 'Stairway to Heaven',
+      peso: 'Led Zeppelin',
+      lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    },
+    zucchinoromanesco = {
+        varietà: 'Stairway to Heaven',
+        peso: 'Led Zeppelin',
+        lunghezza: '1971'
+    }
+
+  ];
+
+
+
+console.log(zucchine)
